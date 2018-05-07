2130b2fe9806be8e2073c882fe03e02fa94abd44
Mira Thoen Feiring <mirafeiring@gmail.com>
2018-05-07 16:45:32 +0200

    forgot to remove some code

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser');
-const mongoDB = require('mongodb');
+const mongodb = require('mongodb');
 
 const PORT = process.env.PORT || 5000
 const ObjectID = mongodb.ObjectID;
-
 const ATTENDING = 'attending';
-
 let db;
 
 const app = express();
@@ -15,46 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 app.get('/', (req, res) => res.render('public/index'))
 
-
 mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test', function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.json())
-  .get('/', (req, res) => res.render('public/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  //
-  //
-  // var express = require("express");
-  // var bodyParser = require("body-parser");
-  // var mongodb = require("mongodb");
-  // var ObjectID = mongodb.ObjectID;
-  //
-  // var CONTACTS_COLLECTION = "contacts";
-  //
-  // var app = express();
-  // app.use(bodyParser.json());
-
-  // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -71,15 +30,12 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
   });
 });
 
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .use(bodyParser.json())
+//   .get('/', (req, res) => res.render('public/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-/*  "/api/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
 
  app.get('/api/attending', function(req, res) {
    db.collection(ATTENDING).find({}).toArray(function(err, docs) {
@@ -91,7 +47,7 @@ function handleError(res, reason, message, code) {
    });
  });
 
- app.post('/api/contacts', function(req, res) {
+ app.post('/api/attending', function(req, res) {
    const newAttendee = req.body;
 
    if (!req.body.name && !req.body.email) {
