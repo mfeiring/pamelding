f615e0aa705b3c5e0f77b2de2eb6c31f79308f19
Mira Thoen Feiring <mirafeiring@gmail.com>
2018-05-09 00:41:08 +0200

    animation bonanza

--- public/index.js
+++ public/index.js
@@ -12,11 +12,47 @@ const handleResponse = (res) => {
   }
 }
 
-document.body.onmousemove = ({screenX: xPos, screenY: yPos}) => {
-  main.style.bottom = `-${(xPos + yPos)/70}px`;
-  main.style.left = `-${(yPos - xPos)/40}px`;
-  signupButton.style.top = `-${xPos/80}px`;
-  signupButton.style.right = `-${yPos/55}px`;
+// $(window).mousemove(function(a) {
+//     var c = a.pageX
+//       , d = a.pageY;
+//     $(window).scrollTop() < $(window).height() && m && (m = !1,
+//     clearInterval(l),
+//     l = setInterval(translate(c, d), 300))
+// }));
+//
+//
+// function translate(a, b) {
+//     $(".slick-active").each(function() {
+//         $(".containedimg").eq(0).css("transform", "translateX(" + .01 * a + "px) translateY(" + .008 * b + "px)"),
+//         $(".containedimg").eq(1).css("transform", "translateX(-" + .01 * a + "px) translateY(" + .015 * b + "px)"),
+//         $(".title-fragment").eq(0).css("transform", "translateX(" + .01 * a + "px) translateY(" + .008 * b + "px)"),
+//         $(".title-fragment").eq(1).css("transform", "translateX(-" + .015 * a + "px) translateY(" + .015 * b + "px)"),
+//         $(".title-fragment").eq(2).css("transform", "translateX(" + .01 * a + "px) translateY(" + .008 * b + "px)")
+//     }),
+//     m = !0
+// }
+
+const translate = (xPos, yPos) => {
+  main.style.transform = `translateX(${.01 * xPos}px) translateY(${.008 * yPos}px)`;
+  signupButton.style.transform = `translateX(-${.01 * xPos}px) translateY(${.015 * yPos}px)`;
+}
+
+let l, m = !0;
+
+
+window.onmousemove = ({screenX: xPos, screenY: yPos}) => {
+  // m && (m = !1,
+  //           clearInterval(l),
+  //           l = setInterval(translate(xPos, yPos), 300))
+  // clearInterval(l);
+  // l = setInterval(translate(xPos, yPos), 300);
+translate(xPos, yPos);
+  // main.style.transform = `translateX(${.01 * xPos}px) translateY(${.008 * yPos}px)`;
+  // signupButton.style.transform = `translateX(-${.01 * xPos}px) translateY(${.015 * yPos}px)`;
+  // main.style.bottom = `-${(xPos + yPos)/70}px`;
+  // main.style.left = `-${(yPos - xPos)/40}px`;
+  // signupButton.style.top = `-${xPos/80}px`;
+  // signupButton.style.right = `-${yPos/55}px`;
 }
 
 showForm = (shouldShow) => {
